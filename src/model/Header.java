5800f2ad56232569204e088ce892096384868d69
mvrlrd <mvrlrd@ya.ru>
2022-09-18 10:11:05 +0300

    added Header.java

---
+++ src/model/Header.java
@@ -0,0 +1,40 @@
+package model;
+
+public class Header {
+    private String text;
+    private int style;
+
+    public Header(String text, int style) {
+        this.text = text;
+        this.style = style;
+    }
+    public Header(String text){
+        this.text=text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public int getStyle() {
+        return style;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void setStyle(int style) {
+        this.style = style;
+    }
+
+    @Override
+    public String toString() {
+    StringBuilder str = new StringBuilder();
+        for (char sign : text.toCharArray()) {
+            str.append(sign);
+            str.append(" ".repeat(Math.max(0, style)));
+        }
+        return str.toString().toUpperCase();
+    }
+}
