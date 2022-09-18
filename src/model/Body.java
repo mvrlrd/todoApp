e80d8ce164379cc6b3baba1d493c9564e8f1bc6d
mvrlrd <mvrlrd@ya.ru>
2022-09-18 10:15:43 +0300

    added Body

---
+++ src/model/Body.java
@@ -0,0 +1,17 @@
+package model;
+
+public class Body {
+    private String text;
+
+    public Body(String text){
+        this.text= text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
